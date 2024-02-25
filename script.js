a8d4b9a9a8694fc959a32bb59160e8cbe201811f
Samosa43 <157168493+Samosa43@users.noreply.github.com>
2024-02-25 17:15:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("input");
+let num = document.querySelectorAll(".num");
+let clr = document.getElementById("clr");
+let del = document.getElementById("del");
+let equal = document.getElementById("equal");
+let displayText = "";
+input.disabled = true;
+
+num.forEach((n)=>{
+    n.addEventListener("click",()=>{
+        displayText = displayText + n.textContent;
+        display();
+    })
+})
+
+clr.addEventListener("click",()=>{
+    displayText = "";
+    display();
+})
+
+del.addEventListener("click",()=>{
+    displayText = displayText.substring(0,displayText.length-1);
+    display();
+})
+
+function display(){
+    input.value = displayText;
+}
+
+equal.addEventListener("click",()=>{
+    if(displayText=="")
+    {
+    displayText = "Please enter the equation";
+    display();
+    }
+    displayText = eval(displayText);
+    display();
+})
+
+
